d25311d85ea70c105ff004ac746402b63c4c616b
kapildev333 <kapssoni333@gmail.com>
2025-08-09 14:54:59 -0400

    Refactor release configuration for semantic release setup

--- release.config.js
+++ release.config.js
@@ -1,10 +1,20 @@
 module.exports = {
-    branches: ["main"], // Specify your release branch
-    plugins: [
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
-        "@semantic-release/changelog",
-        "@semantic-release/github",
-        "@semantic-release/git"
-    ]
+    "name": "soen-6011-final-project",
+    "version": "1.0.0",
+    "devDependencies": {
+        "@semantic-release/changelog": "^6.0.3",
+        "@semantic-release/git": "^10.0.1",
+        "@semantic-release/github": "^9.2.6",
+        "semantic-release": "^22.0.12"
+    },
+    "release": {
+        "branches": ["main"],
+        "plugins": [
+            "@semantic-release/commit-analyzer",
+            "@semantic-release/release-notes-generator",
+            "@semantic-release/changelog",
+            "@semantic-release/github",
+            "@semantic-release/git"
+        ]
+    }
 };
